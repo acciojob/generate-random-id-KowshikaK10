a1457b20e74f541a625e5f95dce8e59219e8c887
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-06-24 21:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let ch='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let ans='';
+	while(l>0){
+		ans+=ch.charAt(Math.floor(Math.random()*ch.length));
+		l--;
+	}
+	return ans;
 }
 
 // Do not change the code below.
